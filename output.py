160025c90d5f13eb802238d97ce16efd61051f07
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2020-05-01 19:03:17 +0100

    Generate plots from the simulations

---
+++ output.py
@@ -0,0 +1,65 @@
+"""
+Run the analyses and generate plots.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import simulate
+import comlag
+
+plt.ion()
+plt.clf()
+
+# Simulate the signals
+dur = 20
+fs = 1000
+t, s_a, s_b = simulate.sim(dur=dur, fs=fs, noise_amp=0.2)
+
+plt.subplot(2, 1, 1)
+plt.plot(t, s_a, label='$s_a$')
+plt.plot(t, s_b, label='$s_b$')
+plt.legend(loc='upper right')
+plt.xlim(1, 2)
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+# Compute CFC using the Tort method
+
+# Which frequencies to calculate phase for
+f_mod_centers = np.logspace(np.log10(2), np.log10(30), 20)
+f_mod_width = f_mod_centers / 6
+f_mod = np.tile(f_mod_width, [2, 1]).T \
+            * np.tile([-1, 1], [len(f_mod_centers), 1]) \
+            + np.tile(f_mod_centers, [2, 1]).T
+
+# Which frequencies to calculate power for
+f_car = np.arange(30, 200, 5)
+
+# CFC within signal A
+mi_a = comlag.cfc_tort(s_a, s_a, fs, f_mod, f_car)
+plt.subplot(2, 3, 4)
+plt.contourf(np.mean(f_mod, axis=1), f_car, mi_a)
+plt.xlabel('Phase freq (Hz)')
+plt.ylabel('Amp freq (Hz)')
+plt.title('CFC within $s_a$')
+plt.colorbar()
+
+# CFC from LF phase-diff between A and B to HG amp in B
+mi = comlag.cfc_phasediff_tort(s_a, s_b, fs, f_mod, f_car)
+plt.subplot(2, 3, 5)
+plt.contourf(np.mean(f_mod, axis=1), f_car, mi['a'])
+plt.xlabel('Phase-diff freq (Hz)')
+#plt.ylabel('Amp freq (Hz)')
+plt.title('CFC: phase-diff to $s_a$')
+plt.colorbar()
+
+plt.subplot(2, 3, 6)
+plt.contourf(np.mean(f_mod, axis=1), f_car, mi['b'])
+plt.xlabel('Phase-diff freq (Hz)')
+#plt.ylabel('Amp freq (Hz)')
+plt.title('CFC: phase-diff to $s_b$')
+plt.colorbar()
+
+
+plt.tight_layout()
