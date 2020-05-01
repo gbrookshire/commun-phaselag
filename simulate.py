0dc236746cb52ec7e3b7eaa307b6d6af87b9be72
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2020-05-01 19:00:23 +0100

    Make params into args

--- simulate.py
+++ simulate.py
@@ -9,24 +9,39 @@ if s_b is at an excitable state and receives a high-gamma burst
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, filtfilt
 from acoustics.generator import white, pink
 
-FSAMPLE = 1000.
-
-def sim(dur):
+def sim(dur=10, fs=1000, noise_amp=0.1,
+        gamma_lag_a=0.015, gamma_lag_a_to_b=0.015):
     """
-    Arguments
-    dur: Duration (in s)
+    Simulate phase-locked communication across frequency bands.
+
+    Parameters
+    ----------
+    dur : float 
+        Duration (in s)
+    fs : float
+        Sampling rate (Hz)
+    noise_amp : float
+        Amplitude of the noise added to the signals
+    gamma_lag_a : float
+        Duration by which gamma activity is lagged from the LF trough w/in s_a
+    gamma_lag_a_to_b : float
+        Duration by which gamma activity in s_a is lagged in s_b
 
     Returns
-    t: Vector of time-points (in s)
-    s_a: Signal at area a
-    s_b: Signal at area b
+    -------
+    t : np.ndarray
+        Vector of time-points (in s)
+    s_a : np.ndarray
+        Signal at area a
+    s_b : np.ndarray
+        Signal at area b
     """
 
-    n_samps = int(dur * FSAMPLE)
-    t = np.arange(n_samps) / FSAMPLE
+    n_samps = int(dur * fs)
+    t = np.arange(n_samps) / fs
 
     #######################################
     # Simulate a source signal in which   #
@@ -34,7 +49,7 @@ def sim(dur):
     #######################################
 
     # Make a low-freq signal with fluctuating frequency
-    a_alpha_sig = osc_var_freq(n_samps, fs=FSAMPLE, low=9, high=11, speed=0.1)
+    a_alpha_sig = osc_var_freq(n_samps, fs=fs, low=9, high=11, speed=0.1)
 
     # Make a high-gamma signal that depends on alpha amplitude
     # Following Jiang et al 2015, NeuroImage
@@ -42,26 +57,21 @@ def sim(dur):
     #    along until it is inhibited by an alpha-pulse.
     gamma_freq = (70, 100) # Hz
     a_gamma_scale = 1 # How strong is the gamma activity
-    a_lf_gamma_lag = -0.005 # Lag the gamma signal (in s) from the LF trough
-    ############### 0.015 is a reasonable number
     a = 10 # Sigmoid slope
     c = 0.1 # Sigmoid "threshold"
     sigmoid = lambda x,a,c: 1 - (1 / (1 + np.exp(-a * (x - c))))
     # a_gamma_osc = bp_noise(n_samps, *gamma_freq)
-    a_gamma_osc = osc_var_freq(n_samps, FSAMPLE, gamma_freq[0], gamma_freq[1], 0.5) - 0.5
+    a_gamma_osc = osc_var_freq(n_samps, fs, gamma_freq[0], gamma_freq[1], 0.5) - 0.5
     a_gamma_sig = sigmoid(a_alpha_sig, a, c) * a_gamma_osc
     a_gamma_sig = np.roll(a_gamma_sig, # Gamma is at a lag from alpha
-                          int(a_lf_gamma_lag * FSAMPLE)) 
+                          int(gamma_lag_a * fs)) 
     a_gamma_sig = a_gamma_sig * a_gamma_scale
 
-    # Add white noise and pink noise
-    white_amp = 0.01
-    pink_amp = 0.01
-    white_noise = white_amp * normalize(white(n_samps))
-    pink_noise = pink_amp * normalize(pink(n_samps))
+    # Add noise
+    noise = noise_amp * normalize(pink(n_samps))
 
-    # Combine LF and HF components
-    s_a = a_alpha_sig + a_gamma_sig + white_noise + pink_noise
+    # Combine LF and HF components (and noise)
+    s_a = a_alpha_sig + a_gamma_sig + noise
     
     ################################
     # Simulate a desination signal #
@@ -69,33 +79,21 @@ def sim(dur):
 
     # Make an alpha signal with fluctuating amplitude
     # This controls excitability in area b.
-    b_alpha_sig = osc_var_freq(n_samps, fs=FSAMPLE, low=6, high=14, speed=0.1)
+    b_alpha_sig = osc_var_freq(n_samps, fs=fs, low=6, high=14, speed=0.1)
 
     # Gamma activity in s_a triggers activity in s_b when are b is in an
     # excitable state. Low alpha leads to excitable state
-    a_b_lag = 0.015 # Lag between activity in s_a and s_b
-    b_gamma_scale = 2.0
-    b_gamma_inp = np.roll(a_gamma_sig, int(a_b_lag * FSAMPLE)) # Gamma input
+    b_gamma_scale = 1.0
+    b_gamma_inp = np.roll(a_gamma_sig, int(gamma_lag_a_to_b * fs)) # Gamma input
     b_gamma_sig = sigmoid(b_alpha_sig, a, c) * b_gamma_inp * b_gamma_scale
 
-    # Add white noise and pink noise
-    white_noise = white_amp * normalize(white(n_samps))
-    pink_noise = pink_amp * normalize(pink(n_samps))
+    # Add noise
+    noise = noise_amp * normalize(pink(n_samps))
 
     # Combine LF and HF components
-    s_b = b_alpha_sig + b_gamma_sig + white_noise + pink_noise
+    s_b = b_alpha_sig + b_gamma_sig + noise
 
     return t, s_a, s_b
-
-
-def plot_signals(t, s_a, s_b):
-    plt.clf()
-    plt.plot(t, s_a)
-    plt.plot(t, s_b)
-    plt.xlim(0, 2)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Amplitude')
-    plt.show()
 
 
 #############################
@@ -106,10 +104,10 @@ def normalize(x):
     return x / np.max(x)
 
 
-def bp_noise(n_samps, low, high):
+def bp_noise(n_samps, fs, low, high):
     """ Band-limited noise
     """
-    band_lim_noise = bp_filter(white(n_samps), low, high, FSAMPLE)
+    band_lim_noise = bp_filter(white(n_samps), low, high, fs)
     return band_lim_noise
 
 
@@ -127,18 +125,22 @@ def bp_filter(data, lowcut, highcut, fs, order=2):
         b, a = butter(order, [low, high], btype='band')
         return b, a
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
+    y = filtfilt(b, a, data)
     return y
 
 
 def osc_var_freq(n_samps, fs, low, high, speed):
     """ An oscillation with variable frequency
 
-    n_samps: Number of samples in the output
-    fs: Sampling rate (Hz)
-    low: Lower frequency for the oscillations
-    high: Upper frequency for the oscillations
-    speed: How quickly the frequency can change
+    Args:
+        n_samps: Number of samples in the output
+        fs: Sampling rate (Hz)
+        low: Lower frequency for the oscillations
+        high: Upper frequency for the oscillations
+        speed: How quickly the frequency can change
+
+    Returns:
+        x: A vector of floats
     """
 
     f = bounded_walk(n_samps, low, high, speed) # instantaneous freq
@@ -167,5 +169,11 @@ def bounded_walk(n_samps, low, high, speed):
 ###########################
 
 if __name__ == '__main__':
-    t, s_a, s_b = sim(10)
-    plot_signals(t, s_a, s_b)
+    t, s_a, s_b = sim()
+    plt.clf()
+    plt.plot(t, s_a)
+    plt.plot(t, s_b)
+    plt.xlim(0, 2)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.show()
