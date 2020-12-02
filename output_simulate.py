5052768f410d42ead9afe6942957bf9364427b59
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2020-12-02 12:10:38 +0000

    Plotting simulated cross-talk and MI: clean up plots

--- output_simulate.py
+++ output_simulate.py
@@ -693,7 +693,7 @@ f_mod = np.tile(f_mod_width, [2, 1]).T \
             + np.tile(f_mod_centers, [2, 1]).T
 
 # Which frequencies to calculate power for
-f_car = np.arange(20, 100, 5)
+f_car = np.arange(20, 100, 10)
 
 # Parameters for the simulated signals
 sim_params = dict(dur=100, fs=1000,
@@ -714,25 +714,24 @@ def mi_fnc(s_a, s_b):
     d = dict(mi=mi, mi_comod=mi_comod, counts=counts)
     return d
 
-def plot_contour(x, **kwargs):
-    plt.contourf(f_mod_centers, f_car, x.T,
-                 levels=np.linspace(0, 1, 50),
-                 **kwargs)
-    cb = plt.colorbar(format='%.2f', ticks=[x.min(), x.max()])
-    cb.ax.set_ylabel('$R^2$')
-    plt.ylabel('HF freq (Hz)')
-    plt.xlabel('Phase freq (Hz)')
-
-
 # Simulate signals with no cross-talk
-t, s_a, s_b = sim(signal_leakage=0, **sim_params)
+t, s_a, s_b = simulate.sim(signal_leakage=0, **sim_params)
 res_no_crosstalk = mi_fnc(s_a, s_b)
 
 # Simulate signals with a lot of cross-talk
-t, s_a, s_b = sim(signal_leakage=0.5, **sim_params)
+t, s_a, s_b = simulate.sim(signal_leakage=0.5, **sim_params)
 res_crosstalk = mi_fnc(s_a, s_b)
 
 # Plot it
+def plot_contour(x, **kwargs):
+    plt.contourf(f_mod_centers, f_car, x.T,
+                 #levels=np.linspace(0, 1, 50),
+                 **kwargs)
+    cb = plt.colorbar(format='%.2f', ticks=[x.min(), x.max()])
+    cb.ax.set_ylabel('Sine-fit amp.')
+    plt.ylabel('HF freq (Hz)')
+    plt.xlabel('Phase freq (Hz)')
+
 plt.figure(figsize=(4, 6))
 
 plt.subplot(2, 1, 1)
@@ -745,5 +744,4 @@ plt.title('Includes cross-talk')
 
 plt.tight_layout()
 
-plt.savefig(
 plt.savefig(f'{plot_dir}mi_comod_cross-talk.png', dpi=300)
