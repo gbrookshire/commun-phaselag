199b30dd19029586a137eca4d7ad80c0b574ba31
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2021-04-15 11:46:29 +0100

    Updates: Compute analyses for different filter BW ratios

--- sbatch_rat.py
+++ sbatch_rat.py
@@ -3,18 +3,23 @@
 Call with:
 for iRat in {0..7}
 do
-    for permType in signal shift
+    for lfRatio in $(seq 1.5 0.5 6)
     do
-        sbatch_submit.py \
-            -s 'source load_python-simulated_rhythmic_sampling.sh' \
-            -i "python sbatch_rat.py $iRat $permType" \
-            -t 72:00:00 -m 10G -c 10 -d ../slurm_results/
+        for hfRatio in $(seq 1.5 0.5 6)
+        do
+            echo $iRat $lfRatio $hfRatio
+            sbatch_submit.py \
+                -s 'source load_python-simulated_rhythmic_sampling.sh' \
+                -i "python sbatch_rat.py $iRat $lfRatio $hfRatio" \
+                -t 2:00:00 -m 5G -c 5 -d ../slurm_results/
+        done
     done
 done
 """
 
 import sys
 import datetime
+import copy
 import numpy as np
 from scipy.io import loadmat
 from scipy import signal
@@ -34,37 +39,18 @@ fnames = ['EEG_speed_allData_Rat17_20120616_begin1.mat',
           'EEG_speed_allData_Rat47_20140923_begin1_CA3_CSC11_CA1_TT3.mat',
           'EEG_speed_allData_Rat31_20140110_begin1_CA3_CSC7_CA1_TT2.mat']
 
-
-## ## Parameters from Jiang
-## ## On 4/9, we decided to use the other parameter set
-## # Low-freq 'modulator' frequencies
-## # Jiang et al (2015): "a choice of 3-5 cycles in relation to the slower
-## # oscillation is sensible"
-## f_mod = np.arange(4, 16)
-## f_mod_bw = f_mod / 2.5 # ~4 cycles
-## 
-## # High-freq 'carrier' frequencies
-## # Jiang et al (2015): "a range of 4 to 6 cycles is appropriate when analyzing
-## # how gamma band power is related to the phase of slower oscillations."
-## f_car = np.arange(30, 150, 10)
-## f_car_bw = f_car / 3 # ~5 cycles
-##
-## downsamp_factor = 5 # 2000 Hz / 5 = 400 Hz
-
-## ## Parameters from Feb 17
-f_mod = np.logspace(np.log10(4), np.log10(20), 15)
-f_mod_bw = f_mod / 2
-f_car = np.arange(20, 150, 10)
-f_car_bw = f_car / 80 * 20 # Keep 20 Hz bandwidth at 80 Hz (~ 7 cycles)
+f_bw_ratios = np.arange(1.5, 6.1, 0.5) # From ~2 to ~10 cycles
+f_mod = np.arange(4, 16)
+f_car = np.arange(30, 150, 5)
 downsamp_factor = 5
 
 # Parameters for the MI phase-lag analysis
-k_perm = 500
+k_perm = 0
 lag_sec = 0.006
 mi_params = dict(f_mod=f_mod,
-                 f_mod_bw=f_mod_bw,
+                 f_mod_bw=None,
                  f_car=f_car,
-                 f_car_bw=f_car_bw,
+                 f_car_bw=None,
                  n_bins=2**3,
                  decimate=None,
                  n_perm_phasebin=0,
@@ -79,11 +65,11 @@ mi_params = dict(f_mod=f_mod,
                  verbose=True)
 
 
-def te_fnc(i_rat, perm_type):
+def te_fnc(i_rat, lf_ratio, hf_ratio):
     """ Helper function for parallel computation
     """
     fn = fnames[i_rat]
-    print(fn)
+    print(fn, lf_ratio, hf_ratio)
 
     # Load the data
     d = loadmat(data_dir + fn)
@@ -95,47 +81,32 @@ def te_fnc(i_rat, perm_type):
         s = [signal.decimate(sig, downsamp_factor) for sig in s]
         fs /= downsamp_factor
 
+    # Get the filters ready
+    mip = copy.deepcopy(mi_params)
+    mip['f_mod_bw'] = mip['f_mod'] / lf_ratio
+    mip['f_car_bw'] = mip['f_car'] / hf_ratio
+
     lag = int(lag_sec * fs)
 
-    if perm_type == 'shift':
-        mi_params['n_perm_shift'] = k_perm
-        mi_params['n_perm_signal'] = 0
-
-    elif perm_type == 'signal':
-        mi_params['n_perm_shift'] = 0
-        mi_params['n_perm_signal'] = k_perm
-
-        # Split the data into epochs for the signal-permuting analysis
-        epoch_dur = 1 # seconds
-        epoch_length = int(epoch_dur * fs) # Samples
-        n_samps_to_keep = len(s[0]) // epoch_length * epoch_length
-        n_splits = n_samps_to_keep / epoch_length
-        s = [np.stack(np.split(sig[:n_samps_to_keep], n_splits), axis=1)
-                for sig in s]
-    elif perm_type == None:
-        mi_params['n_perm_shift'] = 0
-        mi_params['n_perm_signal'] = 0
-    else:
-        raise(NotImplementedError(f"perm_type {perm_type} is not supported"))
-
     te_out = comlag.cfc_phaselag_transferentropy(s[0], s[1],
-                                                fs=fs,
-                                                lag=[lag],
-                                                **mi_params)
+                                                 fs=fs,
+                                                 lag=[lag],
+                                                 **mip)
 
     # Save the data
-    save_fname = f"{data_dir}te/te_{now}_rat{i_rat}_{perm_type}.npz"
+    save_fname = f"te_{now}_rat{i_rat}_lfratio-{lf_ratio}_hfratio-{hf_ratio}.npz"
+    save_fname = f"{data_dir}te/{save_fname}"
     np.savez(save_fname, te=te_out, mi_params=mi_params, lag_sec=lag_sec)
     print(save_fname)
 
 
 def run_single_rat_sbatch():
-    i_rat = sys.argv[1]
-    perm_type = sys.argv[2]
-    assert i_rat.isnumeric(), \
-            f'arg must be the index of the animal, got "{i_rat}"'
-    i_rat = int(i_rat)
-    te_fnc(i_rat, perm_type)
+    i_rat = int(sys.argv[1])
+    lf_ratio = float(sys.argv[2])
+    hf_ratio = float(sys.argv[3])
+    #te_fnc(i_rat, lf_ratio, hf_ratio)
+    msg = f"i_rat: {i_rat}, lf ratio: {lf_ratio:.2f}, hf ratio: {hf_ratio:.2f}"
+    print(msg)
 
 
 def run_all_rats_desktop():
@@ -147,7 +118,7 @@ def run_all_rats_desktop():
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         print('Running single analysis on sbatch')
-        run_single_rat_on_sbatch()
+        run_single_rat_sbatch()
     else:
         print('Running all rat analyses without sbatch')
         run_all_rats_desktop()
