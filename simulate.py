5ebaa2ec089c7af7aa43c255b14741774c0e2511
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2021-04-29 11:52:50 +0100

    Linter

--- simulate.py
+++ simulate.py
@@ -14,8 +14,9 @@ from acoustics.generator import white, pink
 
 plot_dir = '../data/plots/simulated/illustrate/'
 
-alpha_freq = (9, 11) # Hz
-gamma_freq = (70, 100) 
+alpha_freq = (9, 11)  # Hz
+gamma_freq = (70, 100)
+
 
 def sim(dur=10, fs=1000, noise_amp=0.1,
         signal_leakage=0.0,
@@ -28,7 +29,7 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
 
     Parameters
     ----------
-    dur : float 
+    dur : float
         Duration (in s)
     fs : float
         Sampling rate (Hz)
@@ -63,7 +64,7 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
 
     #######################################
     # Simulate a source signal in which   #
-    # alpha pulses inhibit gamma activity #
+    # alpha pulses inhibit gamma activity  #
     #######################################
 
     # Make a low-freq signal with fluctuating frequency
@@ -71,7 +72,7 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
                                low=alpha_freq[0], high=alpha_freq[1],
                                speed=0.1)
 
-    if plot: # Plot some example alpha activity
+    if plot:  # Plot some example alpha activity
         n_samps_to_plot = int(fs / 2)
         plt.figure(figsize=(3, 2))
         plt.plot(a_alpha_sig[:n_samps_to_plot])
@@ -82,13 +83,13 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
     # Following Jiang et al 2015, NeuroImage
     #    This implement a strict inhibitory role of alpha. Gamma just chugs
     #    along until it is inhibited by an alpha-pulse.
-    a_gamma_scale = 1 # How strong is the gamma activity
-    a = 10 # Sigmoid slope
-    c = 0.1 # Sigmoid "threshold"
-    sigmoid = lambda x,a,c: 1 - (1 / (1 + np.exp(-a * (x - c))))
-    a_gamma_osc = osc_var_freq(n_samps, fs, gamma_freq[0], gamma_freq[1], 0.5) - 0.5
+    a_gamma_scale = 1  # How strong is the gamma activity
+    a = 10  # Sigmoid slope
+    c = 0.1  # Sigmoid "threshold"
+    a_gamma_osc = osc_var_freq(n_samps, fs, gamma_freq[0], gamma_freq[1], 0.5)
+    a_gamma_osc -= 0.5
 
-    if plot: # Plot some example gamma activity
+    if plot:  # Plot some example gamma activity
         plt.clf()
         plt.plot(a_gamma_osc[:n_samps_to_plot])
         plt.axis('off')
@@ -109,11 +110,11 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
 
     # Multiply the gamma activity by the sigmoid
     a_gamma_sig = sigmoid(a_alpha_sig, a, c) * a_gamma_osc
-    a_gamma_sig = np.roll(a_gamma_sig, # Gamma is at a lag from alpha
-                          int(gamma_lag_a * fs)) 
+    a_gamma_sig = np.roll(a_gamma_sig,  # Gamma is at a lag from alpha
+                          int(gamma_lag_a * fs))
     a_gamma_sig = a_gamma_sig * a_gamma_scale
 
-    if plot: # Plot the gamma activity with PAC
+    if plot:  # Plot the gamma activity with PAC
         plt.clf()
         plt.plot(a_gamma_sig[:n_samps_to_plot])
         plt.axis('off')
@@ -125,15 +126,14 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
     # Combine LF and HF components (and noise)
     s_a = a_alpha_sig + a_gamma_sig + noise
 
-    if plot: # Plot the resulting signal
+    if plot:  # Plot the resulting signal
         plt.clf()
         plt.plot(s_a[:n_samps_to_plot])
         plt.axis('off')
         plt.savefig(f"{plot_dir}s_a_sender.png")
 
-    
     ################################
-    # Simulate a desination signal #
+    # Simulate a desination signal  #
     ################################
 
     # Make an alpha signal with fluctuating amplitude
@@ -142,18 +142,18 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
 
     # Gamma activity in s_a triggers activity in s_b when are b is in an
     # excitable state. Low alpha leads to excitable state
-    if shared_gamma: # Propagate the gamma from the sender
+    if shared_gamma:  # Propagate the gamma from the sender
         b_gamma_sig = np.roll(a_gamma_sig, int(gamma_lag_a_to_b * fs))
-    else: # Generate independent gamma
+    else:  # Generate independent gamma
         b_gamma_osc = osc_var_freq(n_samps, fs,
                                    gamma_freq[0], gamma_freq[1],
                                    0.5) - 0.5
-        b_gamma_sig = np.roll(b_gamma_osc, int(gamma_lag_a_to_b * fs)) # Gamma input
+        b_gamma_sig = np.roll(b_gamma_osc, int(gamma_lag_a_to_b * fs))
 
     b_gamma_scale = 1.0
     b_gamma_sig = sigmoid(b_alpha_sig, a, c) * b_gamma_sig * b_gamma_scale
 
-    if plot: # Plot the gamma signal in the receiver
+    if plot:  # Plot the gamma signal in the receiver
         plt.clf()
         plt.plot(b_gamma_sig[:n_samps_to_plot])
         plt.axis('off')
@@ -165,14 +165,14 @@ def sim(dur=10, fs=1000, noise_amp=0.1,
     # Combine LF and HF components
     s_b = b_alpha_sig + b_gamma_sig + noise
 
-    if plot: # Plot the resulting signal
+    if plot:  # Plot the resulting signal
         plt.clf()
         plt.plot(s_b[:n_samps_to_plot])
         plt.axis('off')
         plt.savefig(f"{plot_dir}s_b_receiver.png")
 
     #############################
-    # Add noise to both signals #
+    # Add noise to both signals  #
     #############################
 
     # Add each signal to the other, simulating volume conduction
@@ -199,8 +199,8 @@ def sim_lf_coh_plus_noise(dur, fs, lag=0, noise_amp=1, osc_amp=1):
     Result: Alpha-limited phase-dependent communication b/w A & B, with
     directionality haphazard between HF frequencies
     Solution: Shuffling HF info across trials/epochs will lead to a permuted
-    distribution that has similar levels of phase-diff-TE. But when there's real
-    communication, it will be stronger in the shuffled case.
+    distribution that has similar levels of phase-diff-TE. But when there's
+    real communication, it will be stronger in the shuffled case.
     """
     n = int(dur * fs)
     t = np.arange(n) / fs
@@ -222,33 +222,42 @@ def sim_lf_coh_with_pac(dur, fs, lag, noise_amp=1.5, osc_amp=1, gamma_amp=1):
     lf_osc = osc_amp * osc_var_freq(n, fs,
                                     alpha_freq[0], alpha_freq[1],
                                     0.1)
-    a = 10 # Sigmoid slope
-    c = 0.1 # Sigmoid "threshold"
-    sigmoid = lambda x: 1 - (1 / (1 + np.exp(-a * (x - c))))
-    gamma = lambda: osc_var_freq(n, fs, gamma_freq[0], gamma_freq[1], 0.5) - 0.5
-    a_gamma = sigmoid(lf_osc) * gamma()
-    b_gamma = sigmoid(lf_osc) * gamma()
+    a = 10  # Sigmoid slope
+    c = 0.1  # Sigmoid "threshold"
+
+    def gamma():
+        return osc_var_freq(n, fs, gamma_freq[0], gamma_freq[1], 0.5) - 0.5
+
+    a_gamma = sigmoid(lf_osc, a, c) * gamma()
+    b_gamma = sigmoid(lf_osc, a, c) * gamma()
     s_a = lf_osc + (gamma_amp * a_gamma) + (noise_amp * pink(n))
     s_b = lf_osc + (gamma_amp * b_gamma) + (noise_amp * pink(n))
     s_b = np.roll(s_b, lag)
     return t, s_a, s_b
 
 
-def sim_lf_coh_with_hf_comm(dur, fs, lag, noise_amp=1.5, osc_amp=1, gamma_amp=1):
+def sim_lf_coh_with_hf_comm(dur, fs, lag, noise_amp=1.5,
+                            osc_amp=1, gamma_amp=1):
     """
     Two signals with LF coherence and HF communication, but the HF
     communication *does not* depend on the LF phase difference.
     """
     t, s_a, s_b = sim_lf_coh_plus_noise(dur, fs, lag, noise_amp, osc_amp)
-    gamma_sig = osc_var_freq(len(t), fs, gamma_freq[0], gamma_freq[1], 0.5) - 0.5
+    gamma_sig = osc_var_freq(len(t), fs, gamma_freq[0], gamma_freq[1], 0.5)
+    gamma_sig -= 0.5
     gamma_sig *= gamma_amp
     s_a += gamma_sig
     s_b += np.roll(gamma_sig, lag)
     return t, s_a, s_b
 
+
 #############################
-# Signal processing helpers #
+# Signal processing helpers  #
 #############################
+
+def sigmoid(x, a, c):
+    return 1 - (1 / (1 + np.exp(-a * (x - c))))
+
 
 def normalize(x):
     return x / np.max(x)
@@ -293,10 +302,10 @@ def osc_var_freq(n_samps, fs, low, high, speed):
         x: A vector of floats
     """
 
-    f = bounded_walk(n_samps, low, high, speed) # instantaneous freq
-    phi = np.cumsum(2 * np.pi * f / fs) # Change in phase per sample
+    f = bounded_walk(n_samps, low, high, speed)  # instantaneous freq
+    phi = np.cumsum(2 * np.pi * f / fs)  # Change in phase per sample
     x = np.sin(phi)
-    x = (1 / 2) * (x + 1) # Make it vary from 0 to 1
+    x = (1 / 2) * (x + 1)  # Make it vary from 0 to 1
     return x
 
 
@@ -304,7 +313,7 @@ def bounded_walk(n_samps, low, high, speed):
     """ A random walk that is bounded within [low, high], changing at 'speed'
     """
     x_steps = np.random.normal(scale=speed, size=n_samps)
-    x = [np.mean([low, high])] # Initialize at the mean value
+    x = [np.mean([low, high])]  # Initialize at the mean value
     for e in x_steps:
         next_val = x[-1] + e
         if (next_val < low) or (next_val > high):
