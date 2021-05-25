506a88b2724f63a9b68a4e512065327a8affb976
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2021-05-25 15:34:12 +0100

    Update to run phase-bin flipping analysis on Desktop

--- sbatch_rat.py
+++ sbatch_rat.py
@@ -29,8 +29,8 @@ import comlag
 
 now = datetime.datetime.now().strftime("%Y-%m-%d-%H%M")
 
-base_data_dir = '../'  # Bluebear
-# base_data_dir = '/media/geoff/Seagate2TB1/geoff/commun-phaselag/'  # Desktop
+# base_data_dir = '../'  # Bluebear
+base_data_dir = '/media/geoff/Seagate2TB1/geoff/commun-phaselag/'  # Desktop
 data_dir = base_data_dir + 'data/RatData/'
 
 fnames = ['EEG_speed_allData_Rat17_20120616_begin1.mat',
@@ -54,13 +54,14 @@ mi_params = dict(f_mod=f_mod,
                  f_mod_bw=None,
                  f_car=f_car,
                  f_car_bw=None,
-                 n_bins=2**3,
+                 n_bins=10,  # 2**3,
                  decimate=None,
                  n_perm_phasebin=0,
                  n_perm_phasebin_indiv=0,
-                 n_perm_signal=100,
+                 n_perm_signal=0,
                  n_perm_shift=0,
                  min_shift=None, max_shift=None,
+                 perm_phasebin_flip=True,
                  cluster_alpha=0.05,
                  diff_method='both',
                  calc_type=2,
@@ -85,11 +86,11 @@ def te_fnc(i_rat, lf_ratio, hf_ratio):
         s = [signal.decimate(sig, downsamp_factor) for sig in s]
         fs /= downsamp_factor
 
-    # Split the data into epochs
-    epoch_len = int(epoch_dur * fs)
-    n_splits = len(s[0]) // epoch_len
-    sig_len = n_splits * epoch_len
-    s = [np.stack(np.split(sig[:sig_len], n_splits), axis=1) for sig in s]
+    # # Split the data into epochs
+    # epoch_len = int(epoch_dur * fs)
+    # n_splits = len(s[0]) // epoch_len
+    # sig_len = n_splits * epoch_len
+    # s = [np.stack(np.split(sig[:sig_len], n_splits), axis=1) for sig in s]
 
     # Get the filters ready
     mip = copy.deepcopy(mi_params)
@@ -111,25 +112,29 @@ def te_fnc(i_rat, lf_ratio, hf_ratio):
     print(save_fname)
 
 
-def run_single_rat_sbatch():
-    i_rat = int(sys.argv[1])
-    lf_ratio = float(sys.argv[2])
-    hf_ratio = float(sys.argv[3])
-    te_fnc(i_rat, lf_ratio, hf_ratio)
+def run_single_rat_sbatch(i_rat, lf_ratio, hf_ratio):
     msg = f"i_rat: {i_rat}, lf ratio: {lf_ratio:.2f}, hf ratio: {hf_ratio:.2f}"
     print(msg)
+    te_fnc(i_rat, lf_ratio, hf_ratio)
 
 
-def run_all_rats_desktop():
+def run_all_rats_desktop(lf_ratio, hf_ratio):
     for i_rat in range(len(fnames)):
         print(f'Analyzing rat {i_rat}')
-        te_fnc(i_rat, None)
+        te_fnc(i_rat, lf_ratio, hf_ratio)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 4:
         print('Running single analysis on sbatch')
-        run_single_rat_sbatch()
-    else:
+        i_rat = int(sys.argv[1])
+        lf_ratio = float(sys.argv[2])
+        hf_ratio = float(sys.argv[3])
+        run_single_rat_sbatch(i_rat, lf_ratio, hf_ratio)
+    elif len(sys.argv) == 3:
         print('Running all rat analyses without sbatch')
-        run_all_rats_desktop()
+        lf_ratio = float(sys.argv[1])
+        hf_ratio = float(sys.argv[2])
+        run_all_rats_desktop(lf_ratio, hf_ratio)
+    else:
+        print('Not a valid number of args')
